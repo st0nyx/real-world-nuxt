b2e262176b5ef2c9376dec92ada70dc60d3c45ba
Bruno Steinmann <bruno.steinmann@gmx.com>
2020-10-04 15:07:18 +0200

    Setup dummy api, use promise w/axios and error handling

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,35 @@
 <template>
   <div>
     <h1>Events</h1>
+
+    <EventCard
+      v-for="(event, index) in events"
+      :key="index"
+      :event="event"
+      :data-index="index"
+    />
   </div>
 </template>
 
 <script>
+import EventCard from '~/components/EventCard'
 export default {
+  components: { EventCard },
+  asyncData({ $axios, error }) {
+    return $axios
+      .get('http://localhost:3000/events')
+      .then((response) => {
+        return {
+          events: response.data,
+        }
+      })
+      .catch((e) => {
+        error({
+          statusCode: 503,
+          message: 'Unable to fetch events at this time.',
+        })
+      })
+  },
   head() {
     // <-- property used by vue-meta to add header tags
     return {
